514a6a818c209242656986a55c2c23676453e9d4
Jhon Camilo Suaza <jhon04suazasanchez@gmail.com>
2026-05-21 20:44:29 -0500

    test: inyectar error intencional para validacion CI/CD

--- src/test/java/com/sena/practica_jenkins/PracticaJenkinsApplicationTests.java
+++ src/test/java/com/sena/practica_jenkins/PracticaJenkinsApplicationTests.java
@@ -8,6 +8,8 @@ class PracticaJenkinsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// ERROR INTENCIONAL para probar la rama feature/auth-error-outlook
+		org.junit.jupiter.api.Assertions.fail("Fallo simulado para validación de Outlook");
 	}
 
 }
